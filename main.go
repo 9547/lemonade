9a2ce2d692d523b99a5ec7e4c8bb63454c1da976
9547 <nivefive9547@gmail.com>
2020-08-30 21:54:20 +0800

    fix(macosX): set env, or can't copy 中文

--- main.go
+++ main.go
@@ -78,3 +78,20 @@ func Do(c *lemon.CLI, args []string) int {
 func writeError(c *lemon.CLI, err error) {
 	fmt.Fprintln(c.Err, err.Error())
 }
+
+func init() {
+	envs := map[string]string{
+		"LANG":        "zh_CN.UTF-8",
+		"LC_COLLATE":  "en_US.UTF",
+		"LC_CTYPE":    "en_US.UTF-8",
+		"LC_MESSAGES": "en_US.UT",
+		"LC_MONETARY": "en_US.UT",
+		"LC_NUMERIC":  "en_US.UTF",
+		"LC_TIME":     "en_US.UTF-8",
+		"LC_ALL":      "en_US.UTF-8",
+	}
+
+	for k, v := range envs {
+		os.Setenv(k, v)
+	}
+}
